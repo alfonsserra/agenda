6b4405256d72ebd747487b9cc074b8ea67862da5
alfonsserra <alfonsoserra@gmail.com>
2018-12-19 18:54:44 +0100

    Add agenda component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModuleModule } from '../share/shared-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
